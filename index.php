c8cc25a1ef885bbbdcc9a10900dcd3d6b3422dea
Boot-Linux-Port-0418 <Vous@exemple.com>
2019-02-21 12:29:37 +0100

    responsive, liens, script

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     ------------------------------------------------------------------------>
     <link rel="stylesheet" type="text/css" href="public/css/portfolio.css">
     <script src="https://cdn.jsdelivr.net/npm/vue@2.5.22/dist/vue.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 <header>
@@ -41,10 +41,12 @@
     <section>
         <h1 class="texte">Qui-suis je ?</h1>
 
-        <p class="justification">Hello je m’appelle Thècle-Aurélie, je suis curieuse et dynamique, j’aime apprendre de nouvelles choses. Je porte un grand intêret pour tout ce qui est en rapport avec le numérique. Le développement web est pour moi un premier pas vers cette sensibilité. J’ai débuté en faisant des études en design graphique. J’ai découvert à travers cette formation toutes les possiblités qui peuvent en découler et d’autres disciplines qui s’en rapprochent de près ou de loin. Le code ainsi que l’interractivité sont des point qui m’interpellent.</p>
+        <p class="justification">Hello, je m’appelle Thècle-Aurélie, je suis curieuse et dynamique, j’aime apprendre de nouvelles choses. Je porte un grand intérêt pour tout ce qui est en rapport avec le numérique. Le développement web est pour moi un premier pas vers cette sensibilité. J’ai débuté en faisant des études en design graphique. J’ai découvert à travers cette formation toutes les possibilités qui peuvent en découler et d’autres disciplines qui s’en rapprochent de près ou de loin. Le code ainsi que l’interactivité sont des point qui m’interpellent.</p>
     </section>
     
 
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
